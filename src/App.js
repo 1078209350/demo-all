c1460158383431611b72ce28f44b2172ef9284b6
关童 <1078209350@qq.com>
2023-03-20 17:05:25 +0800

    [add]redux练习代码提交

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import fetchJsonp from "fetch-jsonp";
+//import { connect } from 'react-redux';
+import { createStore,combineReducers } from 'redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//所有的reducer
+const initialListState = {
+  list: []
+};
+const userReducer = function(state = initialListState, action) {
+  switch(action.type) {
+    case 'USER_LIST_SUCCESS':
+      return Object.assign({}, state, { list: action.list });
+  }
+  return state;
+};
+const widgetReducer = function(state = initialListState, action) {
+  switch(action.type) {
+    case 'USER_LIST_FAIL':
+      return Object.assign({}, state, { list: action.list });
+  }
+  return state;
+};
+//复合reducer
+const reducers = combineReducers({
+  userState: userReducer,
+  widgetState: widgetReducer
+});
+
+// 建立一个 store 并把 reducer 传递进去 dispatch
+const store = createStore(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      list:[]
+    }
+  }
+  getItems=()=> {
+    let api="http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20";
+    fetchJsonp(api).then((res)=>{
+      //这里对数据进行操作   默认的
+      return res.json();
+    }).then((res)=>{
+      store.dispatch({
+        type: 'USER_LIST_SUCCESS',
+        list: res.result
+      });
+      this.setState({
+        list:store.getState().userState.list
+      })
+    }).catch((err)=>{
+      console.log(err);
+    });
+  }
+
+  render() {
+    return (
+        <div>
+          <button onClick={this.getItems}>你是</button>
+          <div>
+            {
+              this.state.list.map((item,index)=>{
+                return(
+                    <ul key={index}>
+                      <li>{item.aid}</li>
+                      <li>{item.username}</li>
+                      <li>{item.title}</li>
+                    </ul>
+                )
+              })
+            }
+          </div>
+        </div>
+    );
+  }
 }
+
 
 export default App;
