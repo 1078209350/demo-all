99ff6ebc25d86d2de4fe70102d580ab00e14559f
关童 <1078209350@qq.com>
2023-03-21 17:55:18 +0800

    [add]react-demo练习代码提交

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { Bind } from 'lodash-decorators'
 
 function App() {
+    let timer = null;
+
+    //防抖
+    const debounce = () => {
+        let timer = null;
+        return function () {
+            clearTimeout(timer);
+            timer = setTimeout(()=>{
+                console.log(1111);
+            }, 1000)
+        }
+    }
+
+    //节流
+    const throttle = () => {
+        let timer = null;
+        return function () {
+            if(timer){
+                return null;
+            }
+            timer = setTimeout(()=>{
+                console.log(2222);
+                timer = null;
+            }, 2000)
+        }
+    }
+
+    const test = ()=>{
+        console.log("执行了")
+    }
+
+    const handleClick = (e)=>{
+        console.log(e);
+    }
+
+  console.log(process.env)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+          <input type="text" onChange={throttle()} onClick={handleClick}/>
+        哈哈哈哈哈呵呵呵呵
+      </div>
+    </>
   );
 }
 
